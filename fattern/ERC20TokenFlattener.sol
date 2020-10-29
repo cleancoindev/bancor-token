2407d6247eba960e968ae3af39c1ab0a549f4fa1
wangzhiyong <wangzhiyong@chtwm.com>
2020-10-29 19:19:58 +0800

    增加ERC20 token flattener

---
+++ fattern/ERC20TokenFlattener.sol
@@ -0,0 +1,168 @@
+// File: contracts/token/interfaces/IERC20Token.sol
+
+
+pragma solidity 0.6.12;
+
+
+interface IERC20Token {
+    function name() external view returns (string memory);
+    function symbol() external view returns (string memory);
+    function decimals() external view returns (uint8);
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address _owner) external view returns (uint256);
+    function allowance(address _owner, address _spender) external view returns (uint256);
+
+    function transfer(address _to, uint256 _value) external returns (bool);
+    function transferFrom(address _from, address _to, uint256 _value) external returns (bool);
+    function approve(address _spender, uint256 _value) external returns (bool);
+}
+
+// File: contracts/utility/Utils.sol
+
+
+pragma solidity 0.6.12;
+
+
+contract Utils {
+
+    modifier greaterThanZero(uint256 _value) {
+        _greaterThanZero(_value);
+        _;
+    }
+
+    function _greaterThanZero(uint256 _value) internal pure {
+        require(_value > 0, "ERR_ZERO_VALUE");
+    }
+
+    modifier validAddress(address _address) {
+        _validAddress(_address);
+        _;
+    }
+
+    function _validAddress(address _address) internal pure {
+        require(_address != address(0), "ERR_INVALID_ADDRESS");
+    }
+
+    modifier notThis(address _address) {
+        _notThis(_address);
+        _;
+    }
+
+    function _notThis(address _address) internal view {
+        require(_address != address(this), "ERR_ADDRESS_IS_SELF");
+    }
+
+
+
+}
+
+// File: contracts/utility/SafeMath.sol
+
+
+pragma solidity 0.6.12;
+
+
+library SafeMath {
+
+    function add(uint256 _x, uint256 _y) internal pure returns (uint256) {
+        uint256 z = _x + _y;
+        require(z >= _x, "ERR_OVERFLOW");
+        return z;
+    }
+
+
+    function sub(uint256 _x, uint256 _y) internal pure returns (uint256) {
+        require(_x >= _y, "ERR_UNDERFLOW");
+        return _x - _y;
+    }
+
+
+    function mul(uint256 _x, uint256 _y) internal pure returns (uint256) {
+        if (_x == 0)
+            return 0;
+
+        uint256 z = _x * _y;
+        require(z / _x == _y, "ERR_OVERFLOW");
+        return z;
+    }
+
+
+    function div(uint256 _x, uint256 _y) internal pure returns (uint256) {
+        require(_y > 0, "ERR_DIVIDE_BY_ZERO");
+        uint256 c = _x / _y;
+        return c;
+    }
+}
+
+// File: contracts/token/ERC20Token.sol
+
+
+pragma solidity 0.6.12;
+
+
+contract ERC20Token is IERC20Token, Utils {
+    using SafeMath for uint256;
+
+
+    string public override name;
+    string public override symbol;
+    uint8 public override decimals;
+    uint256 public override totalSupply;
+    mapping (address => uint256) public override balanceOf;
+    mapping (address => mapping (address => uint256)) public override allowance;
+
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
+    constructor(string memory _name, string memory _symbol, uint8 _decimals, uint256 _totalSupply) public {
+        require(bytes(_name).length > 0, "ERR_INVALID_NAME");
+        require(bytes(_symbol).length > 0, "ERR_INVALID_SYMBOL");
+
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+        totalSupply = _totalSupply;
+        balanceOf[msg.sender] = _totalSupply;
+    }
+
+    function transfer(address _to, uint256 _value)
+        public
+        virtual
+        override
+        validAddress(_to)
+        returns (bool)
+    {
+        balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);
+        balanceOf[_to] = balanceOf[_to].add(_value);
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value)
+        public
+        virtual
+        override
+        validAddress(_from)
+        validAddress(_to)
+        returns (bool)
+    {
+        allowance[_from][msg.sender] = allowance[_from][msg.sender].sub(_value);
+        balanceOf[_from] = balanceOf[_from].sub(_value);
+        balanceOf[_to] = balanceOf[_to].add(_value);
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value)
+        public
+        virtual
+        override
+        validAddress(_spender)
+        returns (bool)
+    {
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+}
--- scripts/fattern.sh
+++ scripts/fattern.sh
@@ -3,5 +3,6 @@
 truffle-flattener contracts/bancor/BancorFormula.sol >fattern/BancorFormulaFlattener.sol
 truffle-flattener contracts/token/BancorToken.sol >fattern/BancorTokenFlattener.sol
 truffle-flattener contracts/utility/PriceOracle.sol >fattern/PriceOracleFlattener.sol
+truffle-flattener contracts/token/ERC20Token.sol >fattern/ERC20TokenFlattener.sol
 
 sed -i -e 's#// SPDX-License-Identifier: SEE LICENSE IN LICENSE##g' fattern/*.sol
